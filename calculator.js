989073be408f07d0c8eb5a0e7c529d0d2d7fde55
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-05-02 20:45:10 +0200

    primera prueba con Jest, no funciona

--- calculator.js
+++ calculator.js
@@ -1,9 +1,19 @@
 function sum(a, b) {
-  return;
+  if (a === '' ) {
+   a = 0
+  } else if ( b === '') {
+    b = 0
+  }
+  return(a + b);
 }
 
 function subtract(a, b) {
-  return;
+  if (a === '' ) {
+    a = 0
+   } else if ( b === '') {
+     b = 0
+   }
+   return(a - b);
 }
 
 function divide(a, b) {
@@ -14,5 +24,6 @@ function multiply(a, b) {
   return a * b;
 }
 
+sum(3, 5)
 // The following is required to make unit tests work. Please ignore it.
 module.exports = { sum, subtract, divide, multiply };
